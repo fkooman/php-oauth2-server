cf021ee9a9c5a0367d971828f7cb832117895434
François Kooman <fkooman@tuxed.net>
2017-03-01 12:43:28 +0100

    move client config in example to separate file

---
+++ example/clients.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'demo_client' => [
+        'redirect_uri' => 'http://localhost:8081/callback.php',
+        'response_type' => 'code',
+        'display_name' => 'Demo Client',
+        'client_secret' => 'demo_secret',
+    ],
+];
--- example/token.php
+++ example/token.php
@@ -29,15 +29,7 @@ try {
 
     // client "database"
     $getClientInfo = function ($clientId) {
-        $oauthClients = [
-            'demo_client' => [
-                'redirect_uri' => 'http://localhost:8081/callback.php',
-                'response_type' => 'code',
-                'display_name' => 'Demo Client',
-                'client_secret' => 'demo_secret',
-            ],
-        ];
-
+        $oauthClients = require 'clients.php';
         if (!array_key_exists($clientId, $oauthClients)) {
             return false;
         }
