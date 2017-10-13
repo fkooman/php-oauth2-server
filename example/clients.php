24d6de83721aadb18d2ae67df8c6bcf2a8a64f90
François Kooman <fkooman@tuxed.net>
2017-10-13 12:19:34 +0300

    no longer need to provide `response_type` in client data

--- example/clients.php
+++ example/clients.php
@@ -3,7 +3,6 @@
 return [
     'demo_client' => [
         'redirect_uri' => ['http://localhost:8081/callback.php'],
-        'response_type' => 'code',
         'display_name' => 'Demo Client',
         'client_secret' => 'demo_secret',
     ],
